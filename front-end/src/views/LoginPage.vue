b9705cf7651b62a50196a55800ce38674b27de18
writerkang <goodwriterkang@gmail.com>
2020-08-05 11:49:44 +0900

    create front-end scaffold

---
+++ front-end/src/views/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>TaskAgile</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
---
+++ front-end/tests/e2e/specs/login.e2e.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'login test': function (browser) {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL + 'login')
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('h1', 'TaskAgile')
+      .end()
+  }
+
+}
